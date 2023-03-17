5f31efbc474e512c2b595217071f854f83df048b
kingsize-17 <okokokingsize1@gmail.com>
2023-03-16 21:07:54 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * min - main function
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of a char: %lu bytes(s)\n", (unasigned long)sizeof(a));
+printf("size of a int: %lu bytes(s)\n", (unasigned long)sizeof(b));
+printf("size of a long int: bytes(s)\n", (unasinged long)sizeof(c));
+printf(size of a long long int: bytes(s)\n", (unasined long)sizeof(d);
+printf(size of a float: bytes(s)\n", (unasined long)sizeof(f));
+return (0);
+}
