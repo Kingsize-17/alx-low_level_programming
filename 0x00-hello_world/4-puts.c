9da3bcafc83e2a752793fbc2d8fcca26c7d099ba
kingsize-17 <okokokingsize1@gmail.com>
2023-03-16 20:03:53 -0700

    4-put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -Programming is like building a multilingual puzzle
- *return: 0(success)
+ * main - main function
+ * Return: 0 (Success)
  */
-int void main(void);
+int main(void);
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (o);
+return (0);
 }
