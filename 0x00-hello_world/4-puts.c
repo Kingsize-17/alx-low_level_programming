4fa7633bf71dc93a2dda12b3511342c667ce2b36
kingsize-17 <okokokingsize1@gmail.com>
2023-03-16 18:28:24 -0700

    c is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main point - Programming is like building a multilingual puzzle
+ * Return: 0 (success)
+ */
+int(void)
+{
+ puts ("\" Programming is like building a multilingual puzzle");
+ return (0);
+}
