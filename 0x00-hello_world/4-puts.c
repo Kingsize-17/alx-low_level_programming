99598a2f8eff62935feaf33a32069d5d74b0f4c8
kingsize-17 <okokokingsize1@gmail.com>
2023-03-18 05:56:02 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main function
- * Returrn: always 0
+ * main - Entry point
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzel");
 	return (0);
 }
