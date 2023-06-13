d77f4dff4143e6351bba722d813f52a051e26914
kingsize <okokokingsize1@gmail.com>
2023-06-13 14:16:38 +0100

    update

--- 0x01-variables_if_else_while/README.md
+++
@@ -1,3 +0,0 @@
-This project is done during **Full Stack software engineering course, at ALX_School** it aims to study What are the arithmetic operators and how to use them
-What are the logical operators (sometimes called boolean operators) and how to use them
-What the the relational operators and how to use them.
---
+++ alx-low_level_programming/0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * min - main function
+ * return: 0 (Success)
+ */
+int main(void)
+{
+printf("size of a char: %d bytes(s)\n", sizeof(char));
+printf("size of an int: %d bytes(s)\n", sizeof(int));
+printf("size of a long int: %d bytes(s)\n", sizeof(long int));
+printf("size of a long long int: %d bytes(s)\n", sizeof(long long int);
+printf("size of a float: bytes(s)\n", sizeof(float));
+return (0);
+}
